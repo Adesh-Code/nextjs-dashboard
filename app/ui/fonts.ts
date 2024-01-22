1381911da4b7053486bf5ca810b318307d1c238e
adesh <>
2024-01-22 17:50:29 +0530

    added routes

---
+++ app/ui/fonts.ts
@@ -0,0 +1,4 @@
+import { Aladin, Lusitana } from 'next/font/google';
+
+export const aladin = Aladin({weight: '400', subsets: ["latin"]});
+export const lusitana = Lusitana({weight: '400', subsets: ["latin"]});
